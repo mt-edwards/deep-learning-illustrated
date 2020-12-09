a558e8f7cbe2f315d9721feb160dc5cd81953a88
Matthew Edwards <mtedwards@protonmail.com>
2020-12-09 10:59:25 +0000

    vggnet in keras included

---
+++ src/vgg-net-in-keras.py
@@ -0,0 +1,71 @@
+# import dependencies
+import tensorflow
+import tflearn.datasets.oxflower17 as oxflower17
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense
+from tensorflow.keras.layers import BatchNormalization, Dropout
+
+# load data
+X, Y = oxflower17.load_data(one_hot=True)
+
+# model specification
+model = Sequential()
+
+# first conv-pool block
+model.add(Conv2D(64, 3, activation='relu', input_shape=(224, 224, 3)))
+model.add(Conv2D(64, 3, activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(BatchNormalization())
+
+# second conv-pool block
+model.add(Conv2D(128, 3, activation='relu'))
+model.add(Conv2D(128, 3, activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(BatchNormalization())
+
+# thrid conv-pool block
+model.add(Conv2D(256, 3, activation='relu'))
+model.add(Conv2D(256, 3, activation='relu'))
+model.add(Conv2D(256, 3, activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(BatchNormalization())
+
+# fourth conv-pool block
+model.add(Conv2D(512, 3, activation='relu'))
+model.add(Conv2D(512, 3, activation='relu'))
+model.add(Conv2D(512, 3, activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(BatchNormalization())
+
+# fifth conv-pool block
+model.add(Conv2D(512, 3, activation='relu'))
+model.add(Conv2D(512, 3, activation='relu'))
+model.add(Conv2D(512, 3, activation='relu'))
+model.add(MaxPooling2D(2, 2))
+model.add(BatchNormalization())
+
+# dense block
+model.add(Flatten())
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+model.add(Dense(4096, activation='relu'))
+model.add(Dropout(0.5))
+
+# output dense layer
+model.add(Dense(17, activation='softmax'))
+
+# model summary
+model.summary()
+
+# model compilation
+model.compile(loss='categorical_crossentropy',
+              optimizer='adam',
+              metrics=['accuracy'])
+
+# model fitting
+model.fit(X, Y,
+          batch_size=64,
+          epochs=250,
+          verbose=1,
+          validation_split=0.1,
+          shuffle=True)
