d38a79a9f01aa029aa896d3b882c305fb4ddd780
Matthew Edwards <mtedwards@protonmail.com>
2020-12-09 13:53:16 +0000

    transfer learning in keras updated with fitting and evaluation

--- src/transfer-learning-in-keras.py
+++ src/transfer-learning-in-keras.py
@@ -19,7 +19,7 @@ batch_size = 32
 
 # define two data generators
 train_generator = train_datagen.flow_from_directory(
-    directory='./hot-dog-not-hot-dog/train',
+    directory='./data/hot-dog-not-hot-dog/train',
     target_size=(224, 224),
     classes=['hot_dog', 'not_hot_dog'],
     class_mode='categorical',
@@ -27,7 +27,7 @@ train_generator = train_datagen.flow_from_directory(
     shuffle=True,
     seed=42)
 valid_generator = valid_datagen.flow_from_directory(
-    directory='./hot-dog-not-hot-dog/test',
+    directory='./data/hot-dog-not-hot-dog/test',
     target_size=(224, 224),
     classes=['hot_dog', 'not_hot_dog'],
     class_mode='categorical',
@@ -48,8 +48,6 @@ for layer in vgg19.layers:
 # model specification
 model = Sequential()
 model.add(vgg19)
-
-# output dense layer with dropout
 model.add(Flatten(name='flatten'))
 model.add(Dropout(0.5, name="dropout"))
 model.add(Dense(2, activation='softmax', name='dense'))
@@ -62,8 +60,12 @@ model.compile(loss='categorical_crossentropy',
               optimizer='adam',
               metrics=['accuracy'])
 
+# model fitting
 model.fit_generator(train_generator,
                     steps_per_epoch=15,
                     epochs=16,
                     validation_data=valid_generator,
                     validation_steps=15)
+
+# model evaluation
+model.evaluate_generator(valid_generator)
